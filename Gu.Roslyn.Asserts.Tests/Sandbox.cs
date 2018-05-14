68f85d7b7282f0ea84979c8ea9296f168d8760ac
Johan Larsson <johan_gavle1@hotmail.com>
2018-05-14 18:56:25 +0200

    <TargetFrameworks>net461;netstandard2.0</TargetFrameworks>

--- Gu.Roslyn.Asserts.Tests/Sandbox.cs
+++ Gu.Roslyn.Asserts.Tests/Sandbox.cs
@@ -3,7 +3,6 @@
 namespace Gu.Roslyn.Asserts.Tests
 {
     using System;
-    using System.Linq;
     using System.Reflection;
     using NUnit.Framework;
 
@@ -24,24 +23,6 @@ namespace Gu.Roslyn.Asserts.Tests
         {
             var assembly = typeof(Sandbox).GetTypeInfo().Assembly;
             var referencedAssemblies = assembly.GetReferencedAssemblies();
-        }
-
-        [Test]
-        public void Test()
-        {
-            Touch(System.Data.AcceptRejectRule.None);
-            Touch(System.Drawing.Brushes.AliceBlue);
-            Touch(System.Runtime.Serialization.EmitTypeInformation.Always);
-            Touch(System.Numerics.BigInteger.MinusOne);
-            foreach (var assembly in typeof(Sandbox).Assembly.GetReferencedAssemblies().Select(Assembly.Load))
-            {
-                Console.WriteLine($"{assembly.GetName().Name} {assembly.ExportedTypes.First().FullName}");
-            }
-        }
-
-        // ReSharper disable once UnusedParameter.Local
-        private static void Touch(object _)
-        {
         }
     }
 }
